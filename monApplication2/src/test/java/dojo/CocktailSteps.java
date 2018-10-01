9385f836db2e46b1463fddbe7105c1d94ec94cbb
Minemega19 <themega19@gmail.com>
2018-10-01 09:56:13 +0200

    Add files via upload

--- monApplication2/src/test/java/dojo/CocktailSteps.java
+++ monApplication2/src/test/java/dojo/CocktailSteps.java
@@ -1,6 +1,10 @@
-package dojo;  
+package dojo;    
 
-import cucumber.api.*; 
+import cucumber.api.PendingException;  
+import cucumber.api.java.en.Given;  
+import cucumber.api.java.en.Then;  
+import cucumber.api.java.en.When;  
+
 import static org.junit.Assert.*;
 import java.util.List;
 
